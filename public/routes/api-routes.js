e4c9f0e092548cd5c2183845d792f0617de7078b
caroline <chelmore1@bigpond.com>
2023-04-28 16:46:58 +1000

    api-routes

--- public/routes/api-routes.js
+++ public/routes/api-routes.js
@@ -1,25 +1,26 @@
 //dependencies
 const router = require("express").Router();
 const saveData = require("../db/saveData");
+const fs = require("fs");
 
 //get request to display the notes.html page
 router.get("/api/notes", async (req, res) => {
-    const dbJson = await JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-        .res.json(dbJson);
+    const dbNotes = await JSON.parse(fs.readFileSync("./db/db.ntoes", "utf8"));
+        .res.json(dbNotes);
 });
        
 
 
 //Post request 
 router.post("api/notes", (req, res) => {
-    const dbJson = await JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    const dbNotes = await JSON.parse(fs.readFileSync("./db/db.notes", "utf8"));
     const newFeedback = {
         title: req.body.title,
         text: req.body.text,
         id: uuidv4(),
     };
     fs.writeFileSync("./db/db.json", JSON.stringify(dbJson));
-    res.json (dbJson)
+    res.json (dbNotes)
     
 
 });
